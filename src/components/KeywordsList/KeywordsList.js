845dc2ab67745631ab448773dbc089951eeacf90
Thomas Sivertsen <thomass@cerpus.com>
2019-12-11 08:58:17 +0100

    Added setting to let the users exceed the maximum number of keywords

--- src/components/KeywordsList/KeywordsList.js
+++ src/components/KeywordsList/KeywordsList.js
@@ -9,6 +9,7 @@ function KeywordsList() {
     const {
         behaviour: {
             numberOfKeywords = 10,
+            allowExceedOfKeywords,
         },
         translate,
         collectExportValues,
@@ -36,6 +37,10 @@ function KeywordsList() {
         setKeywords([...keywordList]);
     }
 
+    function isAddingDisabled() {
+        return keywordList.length >= numberOfKeywords && allowExceedOfKeywords !== true;
+    }
+
     return (
         <section className={"h5p-keywords-keywordslist"}>
             <h2>{translate('headerKeywords')}</h2>
@@ -57,8 +62,8 @@ function KeywordsList() {
                 className={"h5p-keywords-add-keyword"}
                 aria-labelledby={"addKeywordLabel"}
                 onClick={addKeyword}
-                aria-disabled={keywordList.length >= numberOfKeywords}
-                disabled={keywordList.length >= numberOfKeywords}
+                aria-disabled={isAddingDisabled()}
+                disabled={isAddingDisabled()}
             >
                 <div>
                     <span className={"fa fa-plus"} aria-hidden={true}/>
@@ -69,7 +74,7 @@ function KeywordsList() {
                     </span>
                 </div>
             </button>
-            <div className={"h5p-keywords-keywords-left"}>{translate('keywordsLeft', {':num': numberOfKeywords - keywordList.length})}</div>
+            <div className={"h5p-keywords-keywords-left"}>{translate('keywordsLeft', {':num': Math.max(numberOfKeywords - keywordList.length, 0)})}</div>
         </section>
     );
 }
